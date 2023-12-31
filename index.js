435f9cc8221dccd767a2da1cd73dbba1800fb398
Debadithya Ray Barman <91460817+DREXYOP@users.noreply.github.com>
2023-12-31 21:06:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const { default: { stream } } = require("got");
+const { createWriteStream } = require("fs");
+const { execSync } = require("child_process");
+const url = "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.10/Lavalink.jar"; // This will always pull the latest with soundcloud fix ect ect
+const start = () => {
+  const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
+};
+
+start();
