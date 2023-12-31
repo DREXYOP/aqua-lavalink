4179c08f805d6692d197cf3ebfc7c042b5cb7cd1
Debadithya Ray Barman <91460817+DREXYOP@users.noreply.github.com>
2024-01-01 01:17:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
-## install jabba and sht
-echo Downloading Java 17...
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## get java 17
-jabba install zulu@1.17.0-0
+
 echo Java is installed.
 java -version
 npm i
