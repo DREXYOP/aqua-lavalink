da8319ab27b3e6f95a3b8634cfe0ddddb964fcf7
Debadithya Ray Barman <91460817+DREXYOP@users.noreply.github.com>
2024-01-01 01:19:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { execSync } = require("child_process");
 const url = "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.10/Lavalink.jar"; // This will always pull the latest with soundcloud fix ect ect
 const start = () => {
   const download = stream(url).pipe(createWriteStream('Lavalink.jar'));
+  download.on("finish", () => {
+    execSync("java -jar Lavalink.jar", { stdio: "inherit" });
+  });
 };
 
 start();
